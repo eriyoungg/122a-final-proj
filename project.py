7cb2ff99f67e01cfc184c8621f9fe7beb480e6f8
vicksey <vickyzhang@hotmail.ca>
2025-11-23 15:54:58 -0800

    cmd line basic structure for project.py

--- project.py
+++ project.py
@@ -1,3 +1,4 @@
+import sys
 import mysql.connector
 
 mydb = mysql.connector.connect(
@@ -7,3 +8,26 @@ mydb = mysql.connector.connect(
 )
 
 print(mydb)
+
+def process_stream(stream, fout):
+    for raw in stream:
+        line = raw.strip()
+        if not line:
+            continue
+        
+        function = line[0]
+        params = line[1:]
+
+    pass
+
+if __name__ == '__main__':
+    # Assume input will ALWAYS be in correct format
+    try:
+        while True:
+            line = input()
+            if not line:
+                continue
+            if process_stream([line], sys.stdout) == -1:
+                break
+    except EOFError:
+        pass
